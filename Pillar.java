dab720372eb809354c8ba582cd63e5fff0d8fd79
Jorefice27 <Johnnyorefice27@gmail.com>
2016-12-05 17:51:07 -0600

    Add files via upload

--- Pillar.java
+++ Pillar.java
@@ -20,8 +20,8 @@ public class Pillar {
     
 //    arc.setCenterX(SCENEWIDTH / 2);
 //    arc.setCenterY(SCENEHEIGHT);
-//    arc.setRadiusX(280.0f);
-//    arc.setRadiusY(230.0f);
+//    arc.setRadiusX(240.0f);
+//    arc.setRadiusY(240.0f);
     
     public Pillar(double angle, double dist)
     {
@@ -29,12 +29,28 @@ public class Pillar {
         this.angle = angle;
         int x0 = 1920 / 2;
         int y0 = 1080;
+        int cm = 240 / 80;
         double x = dist * Math.cos(angle);
         double y = dist * Math.sin(angle);
         Circle c = new Circle();
         c.setFill(Color.RED);
         c.setRadius(10);
-        
-        
+        c.setCenterX(x0 + (x * cm));
+        c.setCenterY(y0 + (y * cm));   
+    }
+    
+    public Circle getCircle()
+    {
+        return c;
+    }
+    
+    public double centerX()
+    {
+        return c.getCenterX();
+    }
+    
+    public double centerY()
+    {
+        return c.getCenterY();
     }
 }
